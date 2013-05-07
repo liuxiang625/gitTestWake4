8ba7369132c239034129226bc443986fe8ef70e2
Xiang Liu <liuxiang625@gmail.com>
2013-05-07 15:41:20 -0700

    Model.js modified

---
+++ gitTestWake4/Model.js
@@ -0,0 +1,17 @@
+﻿
+guidedModel =// @startlock
+{
+	Person :
+	{
+		name :
+		{
+			events :
+			{
+				onInit:function(attributeName)
+				{// @endlock
+					this = "John Doe or Jan Doe";
+				}// @startlock
+			}
+		}
+	}
+};// @endlock
